973c9bac8b0dadd29d3c29b1ce4212dbd6118505
Geoffrey Harrison <harrisongeo87@gmail.com>
2020-03-29 17:01:28 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,9 +7,11 @@ def load_library(file)
   parsed_hash = {get_meaning: {}, get_emoticon: {}}
   #keys = parse_file.keys
   
+  pp parsed_hash
+  
   parse_file.each do |meaning|
-    parsed_hash[:get_meaning][meaning][1] = meaning.to_s
-    parsed_hash[:get_emoticon][meaning][0] = meaning[1]
+    parsed_hash[:get_meaning][meaning[1]] = meaning.to_s
+    parsed_hash[:get_emoticon][meaning[0]] = meaning[1]
   end
   parsed_hash
 end
