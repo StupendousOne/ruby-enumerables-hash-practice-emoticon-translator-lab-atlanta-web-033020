59ae8e97a8363e141c21381a8d07ec2bdc17946e
Geoffrey Harrison <harrisongeo87@gmail.com>
2020-03-29 16:26:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,21 @@
 # require modules here
+require "yaml"
 
-def load_library
+def load_library(file)
   # code goes here
+  parse_file = YAML.load_file(file)
+  parsed_hash = {}
+  
+  parse_file.each do |meaning|
+    parsed_hash[:get_meaning] = {meaning[0]
+    
+  end
+  parsed_hash
 end
 
 def get_japanese_emoticon
   # code goes here
+  
 end
 
 def get_english_meaning
